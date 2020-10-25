adbf193e3871503c7d9fe0da8a92855d8da530c9
ArtemLapa <artyom.lapa@gmail.com>
2020-10-25 16:46:06 +0200

    :construction: add solutions first homework

--- dist/js-mudule-cobra.e31bb0bc.js
+++ dist/js-mudule-cobra.e31bb0bc.js
@@ -118,7 +118,75 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"index.js":[function(require,module,exports) {
+/*
+* Case 1
+*/
+// const userName = prompt('What is your name?');
+// alert(`Привет, ${userName}!`);
 
+/*
+* Case 2
+*/
+// const userYear = prompt('В каком году вы родились?', '');
+// const currentYear = 2020;
+// const howYear = currentYear - Number(userYear);
+// alert(howYear);
+
+/*
+* Case 3
+*/
+// const lengthSquare = prompt('Введите длину стороны квадрата', '');
+// const sumSquare = Number(lengthSquare) * 4;
+// alert(`Периметр кдвадрата ${sumSquare}`);
+
+/*
+* Case 4
+*/
+// const userRadius = prompt('Введите радиус окружности', '');
+// const areaSum = Math.PI * (Number(userRadius) ** 2);
+// alert(`Площадь квадрата ${areaSum} см`);
+
+/*
+* Case 5
+*/
+// let userDistance = prompt('Введите расстояние в км', '');
+// userDistance = Number(userDistance);
+// let userTime = prompt('За сколько часов вы хотите преодалеть расстояние? (в часах)', '');
+// userTime = Number(userTime);
+// let userSpeed = userDistance / userTime;
+// let kmSpeed = userSpeed * 36;
+// userSpeed = Math.floor(userSpeed * 100) / 100;
+// alert(`Ваша скорость должна быть: ${userSpeed} км/ч`);
+
+/*
+* Case 6
+*/
+// const dollar = 0.84;
+// alert(`1 доллар = ${dollar} евро`)
+// let userNumber = prompt('Введите сумму в долларах', '');
+// userNumber = Number(userNumber);
+// const euro = userNumber * dollar;
+// alert(`${euro} евро`);
+
+/*
+* Case 7
+*/
+// const oneFile = 820;
+// let usbSize = prompt('Укажите объём USB накопителя в Гб', '');
+// usbSize = Number(usbSize) * 1024;
+// const sumFile = Math.floor(usbSize / oneFile);
+// alert(`На накоптель можно записать ${sumFile} файлов`);
+
+/*
+* Case 8
+*/
+// let sumWallet = prompt('Сколько у вас денег?', '');
+// sumWallet = Number(sumWallet);
+// let priceChocholate = prompt('Сколько стоит шоколадка?', '');
+// priceChocholate = Number(priceChocholate);
+// const sumChocolate = Math.floor(sumWallet / priceChocholate);
+// let shortChange = sumWallet - (sumChocolate * priceChocholate);
+// alert(`Вы можете купить ${sumChocolate} шоколадок \nУ вас осталось ${shortChange} гривен`);
 },{}],"../../../../AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +215,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61244" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59324" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
---
+++ index.js
@@ -0,0 +1,73 @@
+/*
+* Case 1
+*/
+// const userName = prompt('What is your name?');
+// alert(`Привет, ${userName}!`);
+
+/*
+* Case 2
+*/
+// const userYear = prompt('В каком году вы родились?', '');
+// const currentYear = 2020;
+// const howYear = currentYear - Number(userYear);
+// alert(howYear);
+
+/*
+* Case 3
+*/
+// const lengthSquare = prompt('Введите длину стороны квадрата', '');
+// const sumSquare = Number(lengthSquare) * 4;
+// alert(`Периметр кдвадрата ${sumSquare}`);
+
+/*
+* Case 4
+*/
+// const userRadius = prompt('Введите радиус окружности', '');
+// const areaSum = Math.PI * (Number(userRadius) ** 2);
+// alert(`Площадь квадрата ${areaSum} см`);
+
+/*
+* Case 5
+*/
+// let userDistance = prompt('Введите расстояние в км', '');
+// userDistance = Number(userDistance);
+
+// let userTime = prompt('За сколько часов вы хотите преодалеть расстояние? (в часах)', '');
+// userTime = Number(userTime);
+
+// let userSpeed = userDistance / userTime;
+// let kmSpeed = userSpeed * 36;
+// userSpeed = Math.floor(userSpeed * 100) / 100;
+// alert(`Ваша скорость должна быть: ${userSpeed} км/ч`);
+
+/*
+* Case 6
+*/
+// const dollar = 0.84;
+// alert(`1 доллар = ${dollar} евро`)
+// let userNumber = prompt('Введите сумму в долларах', '');
+// userNumber = Number(userNumber);
+// const euro = userNumber * dollar;
+// alert(`${euro} евро`);
+
+/*
+* Case 7
+*/
+// const oneFile = 820;
+// let usbSize = prompt('Укажите объём USB накопителя в Гб', '');
+// usbSize = Number(usbSize) * 1024;
+// const sumFile = Math.floor(usbSize / oneFile);
+// alert(`На накоптель можно записать ${sumFile} файлов`);
+
+/*
+* Case 8
+*/
+// let sumWallet = prompt('Сколько у вас денег?', '');
+// sumWallet = Number(sumWallet);
+
+// let priceChocholate = prompt('Сколько стоит шоколадка?', '');
+// priceChocholate = Number(priceChocholate);
+
+// const sumChocolate = Math.floor(sumWallet / priceChocholate);
+// let shortChange = sumWallet - (sumChocolate * priceChocholate);
+// alert(`Вы можете купить ${sumChocolate} шоколадок \nУ вас осталось ${shortChange} гривен`);
